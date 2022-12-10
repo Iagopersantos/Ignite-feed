c2e691b8e6cc24e9ffaed4f021a81d3e629c96cc
Iago <Iagoper18santos@gmail.com>
2022-12-10 13:28:22 -0300

    first commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/Rocketseat.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Iago Pereira</strong>
+                            <time title="11 de Maio às 08:13" dateTime="2022-05-11 08:13:55">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
